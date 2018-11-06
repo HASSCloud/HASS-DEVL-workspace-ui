119cb9e00534b39bcf9bd0b10a4ed5ce5c43a8f9
James Li <j.li7@griffith.edu.au>
2018-11-06 14:45:46 +1000

    Removing all links from footer in line with website

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,29 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-const footerLinks = [
-  {
-    href: '//hasscloud.net.au/?page_id=42',
-    label: 'About Us',
-  },
-  {
-    href: '//hasscloud.net.au/?page_id=39',
-    label: 'Contact Us',
-  },
-  {
-    href: '//hasscloud.net.au/?page_id=452',
-    label: 'Data Framework',
-  },
-  {
-    href: '//hasscloud.net.au/?page_id=584',
-    label: 'Guides, Case Studies & Recipes',
-  },
-  {
-    href: '//hasscloud.net.au/?page_id=74',
-    label: 'Project Blog',
-  },
-];
-
 export default class Footer extends React.Component {
   render() {
     const footerMain = (
@@ -31,13 +8,6 @@ export default class Footer extends React.Component {
         <Container>
           <Row>
             <Col md="12">
-              <ul>
-                {
-                  footerLinks.map(link => (
-                    <li key={link.href}><a href={link.href} target="_blank" rel="noopener noreferrer" title={link.label}>{link.label}</a></li>
-                  ))
-                }
-              </ul>
             </Col>
           </Row>
         </Container>
